15b9191835867c3b928020eb54cc60bbcc9cf0b1
ISoumyadeepLaha <laha.somu1995@gmail.com>
2022-12-17 16:48:59 +0530

    first Video Fetch Api done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,53 @@
-import React ,{ useState , useEffect} from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCat, setSelectedCat] = useState();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCat}`)
+      .then((data)=>setVideos(data.items));
+  }, [selectedCat]);
+
   return (
-    <Stack sx={{ flexDirection: { sx: "column" , md: "row" }}}>
-      <Box sx={{ height: {sx: "auto", md: "92vh"}, borderRight: "1px solid #3d3d3d",  px: {sx: 0, md:2 },width: "15%"}}>
-        <Sidebar/>
-        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: "#fff"}}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+          width: "15%",
+        }}
+      >
+        <Sidebar selectedCat={selectedCat} setSelectedCat={setSelectedCat} />
+      </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "#fff",
+          }}
+        >
+          {selectedCat}
+          <span style={{ color: "#FC1503", marginLeft: "10px" }}>Videos</span>
+        </Typography>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright &copy; 2022 VideoTube || by Soumyadeep Laha
         </Typography>
+        <Videos videos={videos}/>
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
+import React from "react";
 import "../index.css";
-import { Stack } from '@mui/material';
+import { Stack } from "@mui/material";
 import { categories } from "../utils/Constraints";
-const Sidebar = () => {
+
+const Sidebar = ({ selectedCat, setSelectedCat }) => {
   return (
-    <Stack direction="row" sx={{
-        overflowY:"auto", height: { sx: "auto" , md: "95%"},
-        flexDirection: { md: "column"},
-    }}>
-        {
-            categories.map((cat)=>(
-                <button className="category-btn">
-                    <span>{cat.icon}</span>
-                    <span>{cat.name}</span>
-                </button>
-            ))
-        }
-
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((cat) => (
+        <button
+          className="category-btn"
+          onClick={() => setSelectedCat(cat.name)}
+          style={{
+            background: cat.name === selectedCat && "#FC1503",
+            color: "#fff",
+          }}
+          key={cat.name}
+        >
+          <span
+            style={{
+              color: cat.name === selectedCat ? "#fff" : "red",
+              marginRight: "20px",
+            }}
+          >
+            {cat.icon}
+          </span>
+          <span style={{ opacity: cat.name === selectedCat ? "1" : "0.8" }}>
+            {cat.name}
+          </span>
+        </button>
+      ))}
     </Stack>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
