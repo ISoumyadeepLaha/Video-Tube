3f0bc7b6d06f6242f3a5277365a8358f47773f31
ISoumyadeepLaha <laha.somu1995@gmail.com>
2023-01-04 12:02:43 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import { Box } from "@mui/material";
-import './App.css';
-
-import React from 'react'
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
-import VideoDetails from "./components/VideoDetails";
-import ChannelDetails from "./components/ChannelDetails";
-import SearchFeed from "./components/SearchFeed";
+import React from "react";
+import { AppContext } from "./context/contextAPI";
 
 const App = () => (
-  <BrowserRouter>
-  <Box sx={{ background: "#1f1f1f" }}>
-    <Navbar/>
-    <Routes>
-      <Route path="/" exact element={<Feed/>}/>
-      <Route path="/video/:id" element={<VideoDetails/>}/>
-      <Route path="/channel/:id"  element={<ChannelDetails/>}/>
-      <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
-    </Routes>
-  </Box>
-  </BrowserRouter>
-)
- 
+  <AppContext>
+    <h1 className="">Hello World</h1>
+  </AppContext>
+);
 
-
-export default App
+export default App;
